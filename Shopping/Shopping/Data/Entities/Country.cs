87ffeb92781bec7ae8593c8cc54320c96e53d372
rosa murillo ramirez <rosaledezma@hotmail.com>
2022-06-13 21:31:56 -0600

    Crud Paises

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+
+        public int Id { get; set; }
+        [Display(Name = "País")]
+        [Required(ErrorMessage ="el campo {0} es requerido ")]
+        [MaxLength(50,ErrorMessage ="el campo {0} debe tener máximo {1} caracteres")]
+          public string Name { get; set; }
+
+
+    }
+}
---
+++ Shopping/Shopping/Migrations/20220614030329_AddIndextoCountry.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shopping.Migrations
+{
+    public partial class AddIndextoCountry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_countries_Name",
+                table: "countries",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_countries_Name",
+                table: "countries");
+        }
+    }
+}
